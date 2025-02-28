0bb83a3bb76f85abde2361bb1e42f0557e677dff
Rachel <rachel.wilcock@nesta.org.uk>
2025-02-28 12:05:30 +0000

    Changed st.cache to st.cache_data as deprecated

--- wimd_streamlit_app.py
+++ wimd_streamlit_app.py
@@ -32,7 +32,7 @@ alt.themes.enable("nestafont")
 colours = NESTA_COLOURS
 
 
-@st.cache
+@st.cache_data
 def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv().encode("utf-8")
